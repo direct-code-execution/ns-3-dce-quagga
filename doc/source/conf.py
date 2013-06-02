5303410a3ff0fb36c28030f795e1b0a6db01a259
Hajime Tazaki <tazaki@nict.go.jp>
2013-06-02 20:58:04 +0900

    add latex document build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -177,10 +177,14 @@ htmlhelp_basename = 'ns3dcequagga'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
+#latex_documents = [
+#  ('index', 'ns-3-dce-quagga-manual.tex', u'ns-3 Direct Code Execution (DCE) Quagga Manual',
+#   u'ns-3 project', 'manual'),
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+#latex_logo = '../ns3_html_theme/static/ns-3.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
